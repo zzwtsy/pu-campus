2b2283ee446519c167ad290f5237c256327223e4
zzwtsy <m3226257319@gmail.com>
2022-11-29 16:21:27 +0800

    完善加载配置文件代码

--- src/main/java/cn/zzwtsy/pu/tools/LoadConfig.java
+++ src/main/java/cn/zzwtsy/pu/tools/LoadConfig.java
@@ -1,5 +1,14 @@
 package cn.zzwtsy.pu.tools;
 
+import cn.zzwtsy.pu.PuCampus;
+import cn.zzwtsy.pu.javabean.Command;
+import cn.zzwtsy.pu.javabean.UserConfig;
+import cn.zzwtsy.pu.utils.ConfigHelper;
+
+import java.io.IOException;
+
+import static cn.zzwtsy.pu.tools.MyStatic.*;
+
 /**
  * 加载配置
  *
@@ -7,7 +16,45 @@ package cn.zzwtsy.pu.tools;
  * @since 2022/11/29
  */
 public class LoadConfig {
-    public void loadConfig(){
-        
+    /**
+     * 加载所有配置
+     */
+    public void loadAllConfig() {
+        PuCampus.INSTANCE.getLogger().info("Loading user config file");
+        try {
+            botUserConfig = ConfigHelper.getConfigFromFile(USER_CONFIG_FILE_NAME, UserConfig.class);
+        } catch (IOException e) {
+            PuCampus.INSTANCE.getLogger().error("Loading user config file error", e);
+        }
+        PuCampus.INSTANCE.getLogger().info("Loading command config file");
+        try {
+            botCommand = ConfigHelper.getConfigFromFile(COMMAND_FILE_NAME, Command.class);
+        } catch (IOException e) {
+            PuCampus.INSTANCE.getLogger().error("Loading command config file error", e);
+        }
+    }
+
+    /**
+     * 加载用户配置
+     */
+    public void loadUserConfig() {
+        PuCampus.INSTANCE.getLogger().info("Loading user config file");
+        try {
+            botUserConfig = ConfigHelper.getConfigFromFile(USER_CONFIG_FILE_NAME, UserConfig.class);
+        } catch (IOException e) {
+            PuCampus.INSTANCE.getLogger().error("Loading user config file error", e);
+        }
+    }
+
+    /**
+     * 加载命令配置
+     */
+    public void loadCommandConfig() {
+        PuCampus.INSTANCE.getLogger().info("Loading command config file");
+        try {
+            botCommand = ConfigHelper.getConfigFromFile(COMMAND_FILE_NAME, Command.class);
+        } catch (IOException e) {
+            PuCampus.INSTANCE.getLogger().error("Loading command config file error", e);
+        }
     }
 }
