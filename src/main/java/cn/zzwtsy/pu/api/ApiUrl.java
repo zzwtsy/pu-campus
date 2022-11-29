56d6172f43c36dc19f249a45ec891cf99517fd30
zzwtsy <m3226257319@gmail.com>
2022-11-29 12:10:45 +0800

    添加一些 api url

--- src/main/java/cn/zzwtsy/pu/api/ApiUrl.java
+++ src/main/java/cn/zzwtsy/pu/api/ApiUrl.java
@@ -1,12 +1,10 @@
-package cn.zzwtsy.pu.tools;
+package cn.zzwtsy.pu.api;
 
 /**
  * @author zzwtsy
  * @since 2022/11/28
  */
-public class MyStatic {
-    public static String oauthToken = null;
-    public static String oauthTokenSecret = null;
+public class ApiUrl {
     public static final String LOGIN_URL = "https://pocketuni.net/index.php?app=api&mod=Sitelist&act=login";
     public static final String GET_SCHOOLS_INFO_URL = "https://pocketuni.net/index.php?app=api&mod=Sitelist&act=getSchools";
     public static final String GET_USER_SIGN_IN_EVENT_LIST_URL = "https://pocketuni.net/index.php?app=api&mod=Event&act=myCanSignInEventList&page=1";
