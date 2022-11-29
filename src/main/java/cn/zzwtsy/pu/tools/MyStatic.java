b3d2d71a621b8c17098f9928e60d759233e611f8
zzwtsy <m3226257319@gmail.com>
2022-11-29 16:07:04 +0800

    添加静态常量

---
+++ src/main/java/cn/zzwtsy/pu/tools/MyStatic.java
@@ -0,0 +1,10 @@
+package cn.zzwtsy.pu.tools;
+
+/**
+ * @author zzwtsy
+ * @since 2022/11/29
+ */
+public class MyStatic {
+    public static final String USER_CONFIG_FILE_PATH = "userConfig";
+    public static final String COMMAND_FILE_PATH = "command";
+}
