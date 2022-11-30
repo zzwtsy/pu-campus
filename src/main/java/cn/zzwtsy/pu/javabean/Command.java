f74f94adb7185c2a278f36f661dc611e97fbe6bf
zzwtsy <m3226257319@gmail.com>
2022-11-30 12:50:08 +0800

    添加注释

--- src/main/java/cn/zzwtsy/pu/javabean/Command.java
+++ src/main/java/cn/zzwtsy/pu/javabean/Command.java
@@ -16,64 +16,141 @@ public class Command {
     private String queryActivityDetailById;
     private String getCalendarEventList;
 
+    /**
+     * 获取命令前缀
+     *
+     * @return {@link String}
+     */
     public String getCommandPrefix() {
         return commandPrefix;
     }
 
+    /**
+     * 设置命令前缀
+     *
+     * @param commandPrefix 命令前缀
+     * @return {@link Command}
+     */
     public Command setCommandPrefix(String commandPrefix) {
         this.commandPrefix = commandPrefix;
         return this;
     }
 
+    /**
+     * 获取登录
+     *
+     * @return {@link String}
+     */
     public String getLogin() {
         return login;
     }
 
+    /**
+     * 设置登录
+     *
+     * @param login 登录
+     * @return {@link Command}
+     */
     public Command setLogin(String login) {
         this.login = login;
         return this;
     }
 
+    /**
+     * 获取查询新事件列表
+     *
+     * @return {@link String}
+     */
     public String getQueryNewEventList() {
         return queryNewEventList;
     }
 
+    /**
+     * 设置查询新事件列表
+     *
+     * @param queryNewEventList 查询新事件列表
+     * @return {@link Command}
+     */
     public Command setQueryNewEventList(String queryNewEventList) {
         this.queryNewEventList = queryNewEventList;
         return this;
     }
 
+    /**
+     * 获取查询登录事件列表
+     *
+     * @return {@link String}
+     */
     public String getQuerySignInEventList() {
         return querySignInEventList;
     }
 
+    /**
+     * 在事件列表中设置查询签名
+     *
+     * @param querySignInEventList 查询登录事件列表
+     * @return {@link Command}
+     */
     public Command setQuerySignInEventList(String querySignInEventList) {
         this.querySignInEventList = querySignInEventList;
         return this;
     }
 
+    /**
+     * 获取查询注销事件列表
+     *
+     * @return {@link String}
+     */
     public String getQuerySignOutEventList() {
         return querySignOutEventList;
     }
 
+    /**
+     * 设置查询注销事件列表
+     *
+     * @param querySignOutEventList 查询注销事件列表
+     * @return {@link Command}
+     */
     public Command setQuerySignOutEventList(String querySignOutEventList) {
         this.querySignOutEventList = querySignOutEventList;
         return this;
     }
 
+    /**
+     * 通过id获取查询活动详细信息
+     *
+     * @return {@link String}
+     */
     public String getQueryActivityDetailById() {
         return queryActivityDetailById;
     }
 
+    /**
+     * 按id设置查询活动明细
+     *
+     * @param queryActivityDetailById 按id查询活动详情
+     * @return {@link Command}
+     */
     public Command setQueryActivityDetailById(String queryActivityDetailById) {
         this.queryActivityDetailById = queryActivityDetailById;
         return this;
     }
 
+    /**
+     * 获取日历事件列表
+     *
+     * @return {@link String}
+     */
     public String getGetCalendarEventList() {
         return getCalendarEventList;
     }
 
+    /**
+     * 设置获取日历事件列表
+     *
+     * @param getCalendarEventList 获取日历事件列表
+     * @return {@link Command}
+     */
     public Command setGetCalendarEventList(String getCalendarEventList) {
         this.getCalendarEventList = getCalendarEventList;
         return this;
--- src/main/java/cn/zzwtsy/pu/javabean/UserConfig.java
+++ src/main/java/cn/zzwtsy/pu/javabean/UserConfig.java
@@ -14,46 +14,101 @@ public class UserConfig {
     private String oauthToken;
     private String oauthTokenSecret;
 
+    /**
+     * 获取QQ群号
+     *
+     * @return long
+     */
     public long getGroupId() {
         return groupId;
     }
 
+    /**
+     * 设置QQ群号
+     *
+     * @param groupId QQ群号
+     * @return {@link UserConfig}
+     */
     public UserConfig setGroupId(long groupId) {
         this.groupId = groupId;
         return this;
     }
 
+    /**
+     * 获取管理员qq号
+     *
+     * @return long
+     */
     public long getAdminId() {
         return adminId;
     }
 
+    /**
+     * 设置管理员qq号
+     *
+     * @param adminId 管理员qq号
+     * @return {@link UserConfig}
+     */
     public UserConfig setAdminId(long adminId) {
         this.adminId = adminId;
         return this;
     }
 
+    /**
+     * 获取电子邮件后缀
+     *
+     * @return {@link String}
+     */
     public String getEmailSuffix() {
         return emailSuffix;
     }
 
+    /**
+     * 设置电子邮件后缀
+     *
+     * @param emailSuffix 电子邮件后缀
+     * @return {@link UserConfig}
+     */
     public UserConfig setEmailSuffix(String emailSuffix) {
         this.emailSuffix = emailSuffix;
         return this;
     }
 
+    /**
+     * 获取setOauthToken
+     *
+     * @return {@link String}
+     */
     public String getOauthToken() {
         return oauthToken;
     }
 
+    /**
+     * 设置setOauthToken
+     *
+     * @param oauthToken oauth令牌
+     * @return {@link UserConfig}
+     */
     public UserConfig setOauthToken(String oauthToken) {
         this.oauthToken = oauthToken;
         return this;
     }
 
+    /**
+     * 获取oauthTokenSecret
+     *
+     * @return {@link String}
+     */
     public String getOauthTokenSecret() {
         return oauthTokenSecret;
     }
 
+    /**
+     * 设置oauthTokenSecret
+     *
+     * @param oauthTokenSecret oauth令牌秘密
+     * @return {@link UserConfig}
+     */
     public UserConfig setOauthTokenSecret(String oauthTokenSecret) {
         this.oauthTokenSecret = oauthTokenSecret;
         return this;
