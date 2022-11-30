eb72942cf985ecb3794d3e1d381f5b7abad1eac5
zzwtsy <m3226257319@gmail.com>
2022-11-30 17:13:00 +0800

    添加获取用户Token方法

---
+++ src/main/java/cn/zzwtsy/pu/server/GetToken.java
@@ -0,0 +1,35 @@
+package cn.zzwtsy.pu.server;
+
+import cn.zzwtsy.pu.javabean.UserConfig;
+import cn.zzwtsy.pu.tools.MyHeaders;
+import cn.zzwtsy.pu.tools.MyRequestBody;
+import cn.zzwtsy.pu.utils.HttpHelper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+import static cn.zzwtsy.pu.api.ApiUrl.LOGIN_URL;
+
+/**
+ * 获取令牌
+ *
+ * @author zzwtsy
+ * @since 2022/11/30
+ */
+public class GetToken {
+    /**
+     * @param userName 用户名
+     * @param password 用户密码
+     * @return boolean
+     * @throws IOException ioexception
+     */
+    public boolean getUserToken(String userName, String password) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String sendPost = HttpHelper.sendPost(LOGIN_URL, MyHeaders.baseHeaders(), MyRequestBody.loginBody(userName, password));
+        JsonNode jsonNode = mapper.readTree(sendPost);
+        UserConfig.INSTANCE.setOauthToken(jsonNode.get("content").get("oauth_token").asText());
+        UserConfig.INSTANCE.setOauthTokenSecret(jsonNode.get("content").get("oauth_token_secret").asText());
+        return "".equals(UserConfig.INSTANCE.getOauthToken()) && "".equals(UserConfig.INSTANCE.getOauthTokenSecret());
+    }
+}
