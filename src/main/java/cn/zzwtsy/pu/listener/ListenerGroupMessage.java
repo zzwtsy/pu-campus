05e29b288c4351597db4ba6d32f81d56efb9ffc7
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2022-12-03 18:31:38 +0800

    修复：登陆错误

--- src/main/java/cn/zzwtsy/pu/listener/ListenerGroupMessage.java
+++ src/main/java/cn/zzwtsy/pu/listener/ListenerGroupMessage.java
@@ -1,7 +1,6 @@
 package cn.zzwtsy.pu.listener;
 
 import cn.zzwtsy.pu.PuCampus;
-import cn.zzwtsy.pu.bean.UserCommand;
 import cn.zzwtsy.pu.service.LoginService;
 import cn.zzwtsy.pu.tools.SaveConfig;
 import net.mamoe.mirai.event.EventHandler;
@@ -11,6 +10,9 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
+import static cn.zzwtsy.pu.tools.MyStatic.userCommand;
+import static cn.zzwtsy.pu.tools.MyStatic.userConfig;
+
 /**
  * 监听群消息
  *
@@ -18,7 +20,7 @@ import java.io.IOException;
  * @since 2022/12/01
  */
 public class ListenerGroupMessage extends SimpleListenerHost {
-    private final String LOGIN_URL = UserCommand.INSTANCE.getCommandPrefix() + UserCommand.INSTANCE.getLogin();
+    private final String LOGIN_COMMAND = userCommand.getCommandPrefix() + userCommand.getLogin();
     String message;
     GroupMessageEvent event;
 
@@ -26,9 +28,10 @@ public class ListenerGroupMessage extends SimpleListenerHost {
     private void onEvent(GroupMessageEvent event) {
         this.event = event;
         message = event.getMessage().contentToString();
-        if (message.startsWith(LOGIN_URL)) {
+        if (message.startsWith(LOGIN_COMMAND)) {
             String[] strings = splitMessage(message);
-            String userToken = new LoginService().getUserToken(strings[1], strings[2]);
+            String userName = strings[1] + userConfig.getEmailSuffix();
+            String userToken = new LoginService().getUserToken(userName, strings[2]);
             if (!"true".equals(userToken)) {
                 event.getGroup().sendMessage(userToken);
             } else {
@@ -49,6 +52,6 @@ public class ListenerGroupMessage extends SimpleListenerHost {
      * @return {@link String[]}
      */
     private String[] splitMessage(@NotNull String message) {
-        return message.split("\t");
+        return message.split(" ");
     }
 }
