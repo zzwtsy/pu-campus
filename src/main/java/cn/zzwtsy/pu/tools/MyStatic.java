52db270a34a74ff5700fff803672ca0a114079ff
zzwtsy <m3226257319@gmail.com>
2022-12-04 16:54:59 +0800

    优化：配置文件名称

--- src/main/java/cn/zzwtsy/pu/tools/MyStatic.java
+++ src/main/java/cn/zzwtsy/pu/tools/MyStatic.java
@@ -9,7 +9,7 @@ import cn.zzwtsy.pu.bean.UserConfig;
  */
 public class MyStatic {
     public static final String USER_CONFIG_FILE_NAME = "userConfig";
-    public static final String COMMAND_FILE_NAME = "command";
+    public static final String COMMAND_FILE_NAME = "userCommand";
     public static final String PATH_NAME = "cn.zzwtsy.pu";
     public static UserConfig userConfig;
     public static UserCommand userCommand;
