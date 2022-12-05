eaab458fae584fec603a222729f364e185979d12
zzwtsy <m3226257319@gmail.com>
2022-12-05 16:57:45 +0800

    添加: 包含用户token的请求头

--- src/main/java/cn/zzwtsy/pu/tools/MyHeaders.java
+++ src/main/java/cn/zzwtsy/pu/tools/MyHeaders.java
@@ -2,6 +2,8 @@ package cn.zzwtsy.pu.tools;
 
 import okhttp3.Headers;
 
+import static cn.zzwtsy.pu.tools.MyStatic.userConfig;
+
 /**
  * 请求头
  *
@@ -9,12 +11,27 @@ import okhttp3.Headers;
  * @since 2022/11/30
  */
 public class MyHeaders {
+    private static final String OAUTH_TOKEN = userConfig.getOauthToken();
+    private static final String OAUTH_TOKEN_SECRET = userConfig.getOauthTokenSecret();
+
     public static Headers baseHeaders() {
         return new Headers.Builder()
                 .add("User-Agent", "client:Android version:6.8.71 Product:M2012K11AC OsVersion:12")
                 .add("Host", "pocketuni.net")
                 .add("Accept-Encoding", "gzip")
                 .add("Connection", "Keep-Alive")
+                .add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+                .build();
+    }
+
+    public static Headers tokenHeaders() {
+        return new Headers.Builder()
+                .add("User-Agent", "client:Android version:6.8.71 Product:M2012K11AC OsVersion:12")
+                .add("Host", "pocketuni.net")
+                .add("Accept-Encoding", "gzip")
+                .add("Connection", "Keep-Alive")
+                .add("oauth_token", OAUTH_TOKEN)
+                .add("oauth_token_secret", OAUTH_TOKEN_SECRET)
                 .add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
                 .build();
     }
