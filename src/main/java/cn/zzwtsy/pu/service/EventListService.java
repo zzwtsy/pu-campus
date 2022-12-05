4de7ba3ab346c686c17fa378024a35ca9422c1ac
zzwtsy <m3226257319@gmail.com>
2022-12-05 16:59:40 +0800

    添加: 按日期获取活动列表方法

---
+++ src/main/java/cn/zzwtsy/pu/service/EventListService.java
@@ -0,0 +1,16 @@
+package cn.zzwtsy.pu.service;
+
+/**
+ * 事件列表
+ *
+ * @author zzwtsy
+ * @since 2022/12/05
+ */
+public class EventListService {
+    /**
+     * 获取日历事件列表
+     */
+    public void getCalendarEventList() {
+
+    }
+}
