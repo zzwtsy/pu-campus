62e902a94fbf4ee6ddaac195887ae0fc00bf08bd
zzwtsy <m3226257319@gmail.com>
2022-12-05 16:58:29 +0800

    添加: 获取活动列表的请求体

--- src/main/java/cn/zzwtsy/pu/tools/MyRequestBody.java
+++ src/main/java/cn/zzwtsy/pu/tools/MyRequestBody.java
@@ -3,6 +3,8 @@ package cn.zzwtsy.pu.tools;
 import okhttp3.FormBody;
 import okhttp3.RequestBody;
 
+import static cn.zzwtsy.pu.tools.MyStatic.userConfig;
+
 /**
  * 请求体
  *
@@ -10,11 +12,22 @@ import okhttp3.RequestBody;
  * @since 2022/11/30
  */
 public class MyRequestBody {
+    private static String oauthToken = userConfig.getOauthToken();
+    private static String oauthTokenSecret = userConfig.getOauthTokenSecret();
+
     public static RequestBody loginBody(String userName, String password) {
         return new FormBody.Builder()
                 .add("password", password)
                 .add("email", userName)
                 .add("client", "1")
+                .build();
+    }
+
+    public static RequestBody calendarEventListBody(String date) {
+        return new FormBody.Builder()
+                .add("day", date)
+                .add("oauth_token", oauthToken)
+                .add("oauth_tokenSecret", oauthTokenSecret)
                 .build();
     }
 }
