b4eb2a44a54bfcfd82cd73632768808e6f9202ed
zzwtsy <m3226257319@gmail.com>
2022-12-05 17:06:10 +0800

    优化代码

--- src/main/java/cn/zzwtsy/pu/tools/DataBaseStatic.java
+++ src/main/java/cn/zzwtsy/pu/tools/DataBaseStatic.java
@@ -6,7 +6,6 @@ package cn.zzwtsy.pu.tools;
  */
 public class DataBaseStatic {
     public static final String DRIVER_CLASS_NAME = "org.sqlite.JDBC";
-    //    public static final String DB_FILE_PATH = "data/cn.zzwtsy.pu/pu.db";
-    public static final String DB_FILE_PATH = "pu.db";
+    public static final String DB_FILE_PATH = "data/cn.zzwtsy.pu/pu.db";
     public static final String SQL_CONNECT_URL = "jdbc:sqlite:" + DB_FILE_PATH;
 }
