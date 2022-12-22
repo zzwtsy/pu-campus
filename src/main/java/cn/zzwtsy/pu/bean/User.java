ad7c05f239a8b1a99118a56a24e721ab61656fd0
zzwtsy <m3226257319@gmail.com>
2022-12-22 18:54:15 +0800

    添加 user bean

---
+++ src/main/java/cn/zzwtsy/pu/bean/User.java
@@ -0,0 +1,31 @@
+package cn.zzwtsy.pu.bean;
+
+public class User {
+    private String qqId;
+    private String oauthToken;
+    private String oauthTokenSecret;
+
+    public String getQqId() {
+        return qqId;
+    }
+
+    public void setQqId(String qqId) {
+        this.qqId = qqId;
+    }
+
+    public String getOauthToken() {
+        return oauthToken;
+    }
+
+    public void setOauthToken(String oauthToken) {
+        this.oauthToken = oauthToken;
+    }
+
+    public String getOauthTokenSecret() {
+        return oauthTokenSecret;
+    }
+
+    public void setOauthTokenSecret(String oauthTokenSecret) {
+        this.oauthTokenSecret = oauthTokenSecret;
+    }
+}
