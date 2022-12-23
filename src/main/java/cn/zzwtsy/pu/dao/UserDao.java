2973f3d86abedc3d25d347d757eaa283b1a25c1f
zzwtsy <m3226257319@gmail.com>
2022-12-23 23:12:26 +0800

    添加删除用户代码

--- src/main/java/cn/zzwtsy/pu/dao/UserDao.java
+++ src/main/java/cn/zzwtsy/pu/dao/UserDao.java
@@ -6,7 +6,15 @@ import cn.zzwtsy.pu.database.DataBaseHelper;
 import java.sql.SQLException;
 import java.util.Map;
 
+/**
+ * UserDao
+ *
+ * @author zzwtsy
+ * @since 2022/12/23
+ */
 public class UserDao {
+    private final String tableName = "user";
+
     /**
      * 获取用户信息
      *
@@ -15,7 +23,7 @@ public class UserDao {
      */
     public User getUserByQQId(String qqId) {
         User user = new User();
-        String sql = "SELECT * FROM user WHERE qqId = ?";
+        String sql = "SELECT * FROM " + tableName + " WHERE qqId = ?";
         try {
             Map<Object, Object> queryMap = DataBaseHelper.executeQueryMap(sql, qqId);
             if (queryMap != null) {
@@ -36,11 +44,11 @@ public class UserDao {
      *
      * @param oauthToken       oauthToken
      * @param oauthTokenSecret oauthTokenSecret
-     * @return int
+     * @return 受影响的行数: >=1 添加成功
      */
     public int addUser(String qqId, String oauthToken, String oauthTokenSecret) {
         int status;
-        String sql = "INSERT INTO user (qqId,oauthToken,oauthTokenSecret) VALUES (?,?,?)";
+        String sql = "INSERT INTO " + tableName + " (qqId,oauthToken,oauthTokenSecret) VALUES (?,?,?)";
         try {
             status = DataBaseHelper.executeUpdate(sql, qqId, oauthToken, oauthTokenSecret);
         } catch (SQLException e) {
@@ -50,16 +58,33 @@ public class UserDao {
     }
 
     /**
+     * 删除用户
+     *
+     * @param qqId qq号
+     * @return 受影响的行数: >=1 删除成功
+     */
+    public int delUser(String qqId) {
+        int delStatus;
+        String sql = "DELETE FROM" + tableName + "WHERE qqId = ?";
+        try {
+            delStatus = DataBaseHelper.executeUpdate(sql, qqId);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return delStatus;
+    }
+
+    /**
      * 更新用户信息
      *
      * @param qqId             qq号
      * @param oauthToken       oauthToken
      * @param oauthTokenSecret oauthTokenSecret
-     * @return int 影响的行数
+     * @return 影响的行数: >=1 更新成功
      */
     public int updateUser(String qqId, String oauthToken, String oauthTokenSecret) {
         int status;
-        String sql = "UPDATE user SET oauthToken = ?,oauthTokenSecret = ? WHERE qqId = ?";
+        String sql = "UPDATE " + tableName + " SET oauthToken = ?,oauthTokenSecret = ? WHERE qqId = ?";
         try {
             status = DataBaseHelper.executeUpdate(sql, oauthToken, oauthTokenSecret, qqId);
         } catch (SQLException e) {
