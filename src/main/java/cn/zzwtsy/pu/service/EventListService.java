aa98da0fe53cea83c4f6371088b32445b8a6b32f
zzwtsy <m3226257319@gmail.com>
2022-12-24 20:12:13 +0800

    解析获取到的活动信息,并发送到qq群内

--- src/main/java/cn/zzwtsy/pu/service/EventListService.java
+++ src/main/java/cn/zzwtsy/pu/service/EventListService.java
@@ -2,6 +2,7 @@ package cn.zzwtsy.pu.service;
 
 import cn.zzwtsy.pu.PuCampus;
 import cn.zzwtsy.pu.bean.User;
+import cn.zzwtsy.pu.tools.FormatTime;
 import cn.zzwtsy.pu.tools.MyHeaders;
 import cn.zzwtsy.pu.tools.MyRequestBody;
 import cn.zzwtsy.pu.utils.HttpHelper;
@@ -10,7 +11,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.Date;
+import java.util.regex.Pattern;
 
 import static cn.zzwtsy.pu.api.ApiUrl.CALENDAR_EVENT_LIST_URL;
 
@@ -23,8 +24,15 @@ import static cn.zzwtsy.pu.api.ApiUrl.CALENDAR_EVENT_LIST_URL;
 public class EventListService {
     /**
      * 根据日期获取事件列表
+     *
+     * @param qqId qq号
+     * @param date 日期
+     * @return {@link String}
      */
-    public String getCalendarEventList(String qqId, Date date) {
+    public String getCalendarEventList(String qqId, String date) {
+        if (!checkDateFormat(date)) {
+            return "日期格式错误";
+        }
         ObjectMapper objectMapper = new ObjectMapper();
         String response;
         JsonNode jsonNode;
@@ -34,7 +42,7 @@ public class EventListService {
         try {
             response = HttpHelper.sendPost(CALENDAR_EVENT_LIST_URL,
                     MyHeaders.tokenHeaders(oauthToken, oauthTokenSecret),
-                    MyRequestBody.calendarEventListBody(String.valueOf(date), oauthToken, oauthTokenSecret));
+                    MyRequestBody.calendarEventListBody(date, oauthToken, oauthTokenSecret));
         } catch (IOException e) {
             PuCampus.INSTANCE.getLogger().error("获取活动列表失败", e);
             return "获取活动列表失败";
@@ -55,9 +63,44 @@ public class EventListService {
     /**
      * 内容解析
      *
+     * @param contentNode 内容节点
      * @return {@link String}
      */
     private String contentParse(JsonNode contentNode) {
-        return null;
+        String message = null;
+        for (int i = 0; i < contentNode.size(); i++) {
+            JsonNode tempNode = contentNode.get(i);
+            String eventStatus = tempNode.get("eventStatus").asText();
+            if ("4".equals(eventStatus)) {
+                //活动标题
+                String title = tempNode.get("title").asText();
+                //活动开始时间
+                String sTime = FormatTime.formatUnixTimestamp(tempNode.get("sTime").asLong());
+                //活动结束时间
+                String eTime = FormatTime.formatUnixTimestamp(tempNode.get("eTime").asLong());
+                //报名开始时间
+                String startline = FormatTime.formatUnixTimestamp(tempNode.get("startline").asLong());
+                //报名结束时间
+                String deadline = FormatTime.formatUnixTimestamp(tempNode.get("deadline").asLong());
+                //剩余可参加人数
+                String limitCount = tempNode.get("limitCount").asText();
+                //活动地址
+                String address = tempNode.get("address").asText();
+                message = "活动名称:" + title + "\n"
+                        + "活动地址:" + address + "\n"
+                        + "活动开始时间:" + sTime + "\n"
+                        + "活动结束时间:" + eTime + "\n"
+                        + "报名开始时间:" + startline + "\n"
+                        + "报名结束时间" + deadline + "\n"
+                        + "剩余可参加人数:" + limitCount + "\n"
+                        + "============";
+            }
+        }
+        return message;
+    }
+
+    private boolean checkDateFormat(String date) {
+        String dateFormat = "^\\d{1,4}(-)(1[0-2]|0?[1-9])\\1(0?[1-9]|[1-2]\\d|30|31)$";
+        return Pattern.matches(dateFormat, date);
     }
 }
