510e215385e46909a29cc8b2051ba7f36945771d
zzwtsy <m3226257319@gmail.com>
2022-12-24 22:39:46 +0800

    插件加载时初始化数据库

--- src/main/java/cn/zzwtsy/pu/PuCampus.java
+++ src/main/java/cn/zzwtsy/pu/PuCampus.java
@@ -1,18 +1,18 @@
 package cn.zzwtsy.pu;
 
+import cn.zzwtsy.pu.database.DataBaseHelper;
+import cn.zzwtsy.pu.init.InitDataBase;
 import cn.zzwtsy.pu.listener.ListenerFriendMessage;
 import cn.zzwtsy.pu.listener.ListenerGroupMessage;
 import cn.zzwtsy.pu.tools.CheckConfigFile;
 import cn.zzwtsy.pu.init.InitConfig;
 import cn.zzwtsy.pu.tools.LoadConfig;
 import cn.zzwtsy.pu.tools.SaveConfig;
-import net.mamoe.mirai.console.extension.PluginComponentStorage;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
 import net.mamoe.mirai.event.Event;
 import net.mamoe.mirai.event.EventChannel;
 import net.mamoe.mirai.event.GlobalEventChannel;
-import org.jetbrains.annotations.NotNull;
 
 
 /**
@@ -32,9 +32,9 @@ public final class PuCampus extends JavaPlugin {
     }
 
     @Override
-    public void onLoad(@NotNull PluginComponentStorage $this$onLoad) {
-        boolean check = new CheckConfigFile().check();
-        if (!check) {
+    public void onEnable() {
+        CheckConfigFile checkConfigFile = new CheckConfigFile();
+        if (!checkConfigFile.checkConfigFile()) {
             if (new InitConfig().initConfig()) {
                 getLogger().info("Init Config file successfully,Please modify the config file and try again");
             } else {
@@ -44,10 +44,14 @@ public final class PuCampus extends JavaPlugin {
         } else {
             LoadConfig.loadAllConfig();
         }
-    }
-
-    @Override
-    public void onEnable() {
+        if (!checkConfigFile.checkDataBaseFile()) {
+            getLogger().info("The database file does not exist and the database is being created");
+            DataBaseHelper.registerDataBase();
+            new InitDataBase().initDataBase();
+        } else {
+            getLogger().info("Registering database");
+            DataBaseHelper.registerDataBase();
+        }
         EventChannel<Event> eventChannel = GlobalEventChannel.INSTANCE.parentScope(this);
         eventChannel.registerListenerHost(new ListenerGroupMessage());
         eventChannel.registerListenerHost(new ListenerFriendMessage());
