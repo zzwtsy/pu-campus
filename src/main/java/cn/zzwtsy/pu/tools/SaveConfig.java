44ad072d5157618e82d4036bcee8451d8c8be4c8
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2022-12-27 22:31:53 +0800

    优化代码

--- src/main/java/cn/zzwtsy/pu/tools/SaveConfig.java
+++ src/main/java/cn/zzwtsy/pu/tools/SaveConfig.java
@@ -1,6 +1,8 @@
 package cn.zzwtsy.pu.tools;
 
 import cn.zzwtsy.pu.PuCampus;
+import cn.zzwtsy.pu.bean.Command;
+import cn.zzwtsy.pu.bean.Setting;
 import cn.zzwtsy.pu.utils.ConfigHelper;
 
 import java.io.IOException;
@@ -38,9 +40,9 @@ public class SaveConfig {
     /**
      * 保存用户配置
      */
-    public static boolean saveSettingConfig(Object object) {
+    public static boolean saveSettingConfig(Setting setting) {
         try {
-            ConfigHelper.setConfigFile(PATH_NAME, SETTING_FILE_NAME, object);
+            ConfigHelper.setConfigFile(PATH_NAME, SETTING_FILE_NAME, setting);
         } catch (IOException e) {
             PuCampus.INSTANCE.getLogger().error("Save setting config file failed", e);
             return false;
@@ -51,9 +53,9 @@ public class SaveConfig {
     /**
      * 保存命令配置
      */
-    public static boolean saveCommandConfig(Object object) {
+    public static boolean saveCommandConfig(Command command) {
         try {
-            ConfigHelper.setConfigFile(PATH_NAME, COMMAND_FILE_NAME, object);
+            ConfigHelper.setConfigFile(PATH_NAME, COMMAND_FILE_NAME, command);
         } catch (IOException e) {
             PuCampus.INSTANCE.getLogger().error("Save command config file failed", e);
             return false;
