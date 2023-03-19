2d4764f65318316f51e5aac7c1a4016bba486c2f
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2023-03-19 14:43:26 +0800

    使用 kotlin 重构代码

---
+++ src/main/kotlin/cn/zzwtsy/pu/utils/DateUtilKt.kt
@@ -0,0 +1,98 @@
+package cn.zzwtsy.pu.utils
+
+import java.time.*
+import java.time.format.DateTimeFormatter
+
+object DateUtilKt {
+    /**
+     * unix 时间戳格式化
+     * @param [unixTimestamp] unix时间戳
+     * @return [String]
+     */
+    @JvmStatic
+    fun unixTimestampToDateTime(unixTimestamp: Long): String {
+        // 将 Unix 时间戳转换成 Instant 对象
+        val instant = Instant.ofEpochSecond(unixTimestamp)
+        // 获取系统默认时区
+        val zoneId = ZoneId.systemDefault()
+        // 将 Instant 对象转换成 LocalDateTime 对象
+        val localDateTime = LocalDateTime.ofInstant(instant, zoneId)
+        // 定义日期时间格式
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+        // 将 LocalDateTime 对象格式化为字符串
+        return localDateTime.format(formatter)
+    }
+
+    /**
+     * 日期时间转换为 unix 时间戳
+     * @param [dateTime] 日期时间
+     * @return [Long]
+     */
+    @JvmStatic
+    fun dateTimeToUnixTimestamp(dateTime: String): Long {
+        // 定义日期时间格式
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+        // 将字符串解析成 LocalDateTime 对象
+        val localDateTime = LocalDateTime.parse(dateTime, formatter)
+        // 获取系统默认时区
+        val zoneId = ZoneId.systemDefault()
+        // 将 LocalDateTime 对象转换成 Instant 对象
+        val instant = localDateTime.atZone(zoneId).toInstant()
+        // 返回 Unix 时间戳
+        return instant.epochSecond
+    }
+
+    /**
+     * 日期计算（天）
+     * @param [date] 日期
+     * @param [days] 天
+     * @return [LocalDate]
+     */
+    @JvmStatic
+    fun daysCalculation(date: LocalDate, days: Int): LocalDate {
+        return date.plusDays(days.toLong())
+    }
+
+    /**
+     * 日期计算（周）
+     * @param [date] 日期
+     * @param [weeks] 周
+     * @return [LocalDate]
+     */
+    @JvmStatic
+    fun weeksCalculation(date: LocalDate, weeks: Int): LocalDate {
+        return date.plusWeeks(weeks.toLong())
+    }
+
+    /**
+     * 日期计算（月）
+     * @param [date] 日期
+     * @param [months] 月
+     * @return [LocalDate]
+     */
+    @JvmStatic
+    fun monthsCalculation(date: LocalDate, months: Int): LocalDate {
+        return date.plusMonths(months.toLong())
+    }
+
+    /**
+     * 日期计算（年）
+     * @param [date] 日期
+     * @param [years] 年
+     * @return [LocalDate]
+     */
+    @JvmStatic
+    fun yearsCalculation(date: LocalDate, years: Int): LocalDate {
+        return date.plusYears(years.toLong())
+    }
+
+    /**
+     * 加一年
+     * @param [date] 日期
+     * @return [String]
+     */
+    @JvmStatic
+    fun makeUpForTheWholeYear(date: String): String {
+        return Year.now().toString() + "-" + date
+    }
+}
--- src/main/kotlin/cn/zzwtsy/pu/utils/EncryptionKt.kt
+++ src/main/kotlin/cn/zzwtsy/pu/utils/EncryptionKt.kt
@@ -9,22 +9,20 @@ import java.security.MessageDigest
  */
 object EncryptionKt {
     /**
-     * md5
-     * @param [content] 内容
-     * @param [salt] 盐
-     * @return [String]
+     * 使用盐值计算字符串的 MD5 值
+     * @param [input] 待加密的字符串
+     * @param [salt] salt 盐值
+     * @return [String] 字符串的 MD5 值
      */
     @JvmStatic
-    fun toMd5(content: String, salt: String): String {
-        val hash = MessageDigest.getInstance("MD5").digest((content + salt).toByteArray())
-        val hex = StringBuilder(hash.size * 2)
-        for (b in hash) {
-            var str = Integer.toHexString(b.toInt())
-            if (b < 0x10) {
-                str = "0$str"
-            }
-            hex.append(str.substring(str.length - 2))
-        }
-        return hex.toString()
+    fun toMd5(input: String, salt: String): String {
+        // 获取 MessageDigest 实例
+        val md = MessageDigest.getInstance("MD5")
+        // 将字符串和盐值拼接在一起，然后转换成字节数组
+        val byteArray = (input + salt).toByteArray(Charsets.UTF_8)
+        // 计算 MD5 值
+        val digest = md.digest(byteArray)
+        // 将结果转换成十六进制字符串
+        return digest.joinToString("") { "%02x".format(it) }
     }
 }
